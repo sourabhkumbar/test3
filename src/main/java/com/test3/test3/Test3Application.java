6de7e7208041022ee19cf63cfe0d7da39b481385
sourabhkumbar <sourabhkumbar99@gmail.com>
2025-04-08 21:00:05 +0530

    Update Test3Application.java

--- src/main/java/com/test3/test3/Test3Application.java
+++ src/main/java/com/test3/test3/Test3Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Test3Application {
-double  z = 10.3;
+double z = 10.3;
 	public static void main(String[] args) {
 		SpringApplication.run(Test3Application.class, args);
 	}
